cf42cdab3a2cc734adfa9ac6788b061bbb91d566
Cazajous Miguel A <migue7490@gmail.com>
2021-01-29 20:58:10 -0300

    Removed input

--- Hackerrank/Haskell/a_very_big_sum.hs
+++ Hackerrank/Haskell/a_very_big_sum.hs
@@ -3,8 +3,5 @@
 bigSum :: [String] -> Int 
 bigSum xs = sum (map read xs :: [Int])
 
-input :: String
-input = "5\n1000000001 1000000002 1000000003 1000000004 1000000005\n"
-
 main :: IO ()
 main = interact $ show . bigSum . tail . words
